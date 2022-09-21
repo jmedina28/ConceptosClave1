08ef6283f5c6ce97f986030f25122c7f88783e8b
Juan Medina <91565820+jmedina28@users.noreply.github.com>
2022-09-21 17:30:49 +0200

    paso todo al main.py

--- plano.py
+++ plano.py
@@ -1,6 +1,5 @@
 from re import X
 
-
 class Punto:
     def __init__(self, x, y):
         self.x = x
@@ -35,21 +34,6 @@ class Punto:
     def distancia(self, punto):
         return ((self.x - punto.x)**2 + (self.y - punto.y)**2)**0.5
    
-x1 = 0
-y1 = 0
-
-print(Punto(x1,y1).pantalla())
-print(Punto(x1,y1).cuadrante())
-
-x2 = 5
-y2 = 5
-
-print(Punto(x2,y2).pantalla())
-print(Punto(x2,y2).cuadrante())
-
-print("El vector que forman los dos puntos es el siguiente: " + str(Punto(x1,y1).vector(Punto(x2,y2)).pantalla()))
-print("La distancia entre los dos puntos es de: " + str(Punto(x1,y1).distancia(Punto(x2,y2))))
-
 class Rectangulo:
     def __init__(self, punto1, punto2):
         self.punto1 = punto1
@@ -63,28 +47,3 @@ class Rectangulo:
     
     def altura(self):
         return abs(self.punto1.y - self.punto2.y)
-
-x1 = 0
-y1 = 0
-print(Punto(x1,y1).pantalla())
-x2 = 5
-y2 = 5
-print(Punto(x2,y2).pantalla())
-print("El área del rectángulo es: " + str(Rectangulo(Punto(x1,y1), Punto(x2,y2)).area()))
-print("La base del rectángulo mide: " + str(Rectangulo(Punto(x1,y1), Punto(x2,y2)).base()))
-print("La altura del rectángulo mide: " + str(Rectangulo(Punto(x1,y1), Punto(x2,y2)).altura()))
-
-A = Punto(2,3)
-B = Punto(5,5)
-C = Punto(-3,-1)
-D = Punto(0,0)
-print(A.pantalla()), print(A.cuadrante())
-print(B.pantalla()), print(B.cuadrante())
-print(C.pantalla()), print(C.cuadrante())
-print(D.pantalla()), print(D.cuadrante())
-print(A.vector(B).pantalla())
-print(B.vector(A).pantalla())
-print(A.distancia(B))
-print(B.distancia(A))
-# Rectángulo con vértices A y B
-print(Rectangulo(A, B).area())
